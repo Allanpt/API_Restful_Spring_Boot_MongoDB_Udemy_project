9fbc251b006e1885138fd28e4c7be3496b7344ed
Allanpt <jatallantavares@gmail.com>
2023-07-28 10:25:20 -0300

    Initial commit

---
+++ API_Restful_Spring_Boot_MongoDB_Udemy_project/src/main/java/com/project/API_Restful_Spring_Boot_MongoDB_Udemy_project/ApiRestfulSpringBootMongoDbUdemyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.API_Restful_Spring_Boot_MongoDB_Udemy_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulSpringBootMongoDbUdemyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulSpringBootMongoDbUdemyProjectApplication.class, args);
+	}
+
+}
---
+++ API_Restful_Spring_Boot_MongoDB_Udemy_project/src/test/java/com/project/API_Restful_Spring_Boot_MongoDB_Udemy_project/ApiRestfulSpringBootMongoDbUdemyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.API_Restful_Spring_Boot_MongoDB_Udemy_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulSpringBootMongoDbUdemyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
