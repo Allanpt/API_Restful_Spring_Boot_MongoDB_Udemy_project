78a51c1cfdbb8d58e1e202a94479e0d9a05b01ce
Allanpt <jatallantavares@gmail.com>
2023-07-29 08:12:04 -0300

    UserDTO implement

--- API_Restful_Spring_Boot_MongoDB_Udemy_project/src/main/java/com/project/API_Restful_Spring_Boot_MongoDB_Udemy_project/resources/UserResource.java
+++ API_Restful_Spring_Boot_MongoDB_Udemy_project/src/main/java/com/project/API_Restful_Spring_Boot_MongoDB_Udemy_project/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.project.API_Restful_Spring_Boot_MongoDB_Udemy_project.resources;
 
 import com.project.API_Restful_Spring_Boot_MongoDB_Udemy_project.domain.User;
+import com.project.API_Restful_Spring_Boot_MongoDB_Udemy_project.dto.UserDTO;
 import com.project.API_Restful_Spring_Boot_MongoDB_Udemy_project.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -19,8 +21,9 @@ public class UserResource {
     private UserService service;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll(){
         List<User> userList = service.findAll();
-        return ResponseEntity.ok().body(userList);
+        List<UserDTO> dtoList = userList.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(dtoList);
     }
 }
